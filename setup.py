120ce05746379ec233646110a997b181f01d931f
Manuel Guenther <manuel.guenther@idiap.ch>
2012-12-11 16:51:29 +0100

    Changed version to 1.0.0 to upload package to PyPI.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.mobio',
-    version='1.0.0a1',
+    version='1.0.0',
     description='MOBIO Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.mobio',
     license='GPLv3',
