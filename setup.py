46ee67c8461c0ed2efaaac8a2fb4166afcd6bcaf
Manuel Guenther <manuel.guenther@idiap.ch>
2013-02-13 13:45:59 +0100

    Switched to the latest buildout version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.mobio',
-    version='1.0.2',
+    version='1.0.3',
     description='MOBIO Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.mobio',
     license='GPLv3',
