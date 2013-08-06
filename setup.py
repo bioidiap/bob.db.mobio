0556c77be993814e1f3c7361c6d7d2b16c1d2449
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-06 12:53:02 +0200

    Update bootstrap and version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.mobio',
-    version='1.0.3',
+    version='1.0.4',
     description='MOBIO Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.mobio',
     license='GPLv3',
