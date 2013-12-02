c8f84c29a7b4fa48db93bd162d801d6b87a9092a
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-12-02 13:03:51 +0100

    Update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.mobio',
-    version='1.2.1',
+    version='1.2.2',
     description='MOBIO Database Access API for Bob',
     url='https://pypi.python.org/pypi/xbob.db.mobio',
     license='GPLv3',
